95c70606c901762c7e423e86854e2d8deaea7e85
zhaolinmao <121776554@qq.com>
2020-04-19 22:13:26 +0800

    path

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,13 @@ Vue.use(VueRouter);
 /* Layout */
 import Layout from '@layout'
 
-const routes = [
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+export const constantRoutes = [
   {
     path: "/login",
     name: "Home",
@@ -29,10 +35,22 @@ const routes = [
   }
 ];
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
+export const asyncRoutes = [
+	
+];
 
-export default router;
+const createRouter = () => new VueRouter({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router

c7e880ae7cf791cde346c8e8f26d417ccc453f7c
zhaolinmao <121776554@qq.com>
2020-04-21 09:51:58 +0800

    normalize

--- vue.config.js
+++ vue.config.js
@@ -29,7 +29,7 @@ module.exports = {
             .set('@', resolve('src'))
             .set('@api', resolve('src/api'))
             .set('@assets', resolve('src/assets'))
-            .set('@style', resolve('src/assets/style'))
+            .set('@styles', resolve('src/assets/styles'))
             .set('@fonts', resolve('src/assets/fonts'))
             .set('@comp', resolve('src/components'))
             .set('@layout', resolve('src/layout'))

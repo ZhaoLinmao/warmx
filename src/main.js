e6951c0d6d1c26460e08355eae0f4403ad6a8d19
zhaolinmao <121776554@qq.com>
2020-04-21 09:50:29 +0800

    导入index.css全局css

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from "vue";
 
 import Cookies from 'js-cookie'
 
+// 统一浏览器初始化css样式
+import 'normalize.css'
+
 // 导入elementUI的js包
 import Element from 'element-ui';
 // 导入elementUI的css插件
 import 'element-ui/lib/theme-chalk/index.css';
 // alibaba的 font 图标字体库
 import '@fonts/iconfont.css';
+//全局css文件
+import '@styles/index.scss'
 
 //导入语言包
 import local from 'element-ui/lib/locale/lang/zh-CN'
